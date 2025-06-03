12ab8133705b9230213a8a72f5adf82aed582a15
Abraão Moreira <abraaol.moreira@outlook.com>
2025-06-03 11:15:17 -0300

    feat(config): reorganize nuxt.config.ts by removing unused imports and adjusting devServer configuration

--- Antonieta.Frontend/nuxt.config.ts
+++ Antonieta.Frontend/nuxt.config.ts
@@ -1,13 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import AutoImport from 'unplugin-auto-import/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
-
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       backendUrl: process.env.BACK_BASE_URL || 'http://localhost:8000'
     }
+  },
+  devServer: {
+    port: Number(process.env.FRONTEND_PORT) || 5000,
   },
   devtools: { enabled: true },
   modules: ['@bg-dev/nuxt-naiveui'],
